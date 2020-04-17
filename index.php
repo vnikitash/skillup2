bdd4298bd95f36ae575abbc0d5c58a3019772c36
Viktor <viktor@stryber.com>
2020-04-17 13:40:36 +0300

    new tasl

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
 <?php
 
-require_once "libs/Smarty.class.php";
-require_once "functions.php";
+$db = new mysqli('127.0.0.1', 'root', '', 'test');
 
-$smarty = new Smarty();
-$smarty->setTemplateDir('templates');
+$res = $db->query("SELECT * FROM users");
+$users = $res->fetch_assoc();
 
-$action = $_GET['action'] ?? 'index';
-
-switch ($action) {
-    case "create":
-
-        print_r($_POST['test']);
-        die();
-
-        $login = $_POST['login'] ?? null;
-        $password = $_POST['password'] ?? null;
-        if ($login && $password) {
-            createUser($login, $password);
-        }
-        header('Location: /');
-        break;
-    case "delete":
-        if (!isset($_POST['id'])) {
-            die("We do not know which user we should remove");
-        }
-
-        removeUser($_POST['id']);
-        header('Location: /');
-        break;
-    case "index":
-        showMain();
-}
+print_r($users);
--- mysqldump.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: viktor
- * Date: 15.04.2020
- * Time: 18:45
- */
